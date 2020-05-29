810254529cbda2aa9d8a5559ee1bfb68ac415445
subinrsa <subinrsa@gmail.com>
2020-05-28 23:21:51 -0500

    Update dependencies

--- src/test/java/rest/assured/AppTest.java
+++ src/test/java/rest/assured/AppTest.java
@@ -4,10 +4,12 @@
 package rest.assured;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-
+        assertThat(1, is(1));
             }
 }
