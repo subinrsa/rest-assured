cbda571534c30b01f4059c516b26afbe46af2133
subinrsa <subinrsa@gmail.com>
2020-05-30 00:32:01 -0500

    Creating api tests

--- src/test/java/rest/assured/AppTest.java
+++ src/test/java/rest/assured/AppTest.java
@@ -1,12 +1,48 @@
 package rest.assured;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void test() {
-        assertThat(1, is(1));
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    public void testListUserMetadata() {
+        when().
+            get("https://reqres.in/api/users?page=2").
+        then().
+            statusCode(HttpStatus.SC_OK)
+            .body("page", is(2))
+            .body("data", is(notNullValue()));
         }
+
+    @Test
+    public void testCreateUserSuccess() {
+        given().
+            contentType(ContentType.JSON)
+            .body("{ \"name\": \"test\", \"job\": \"QA\" }").
+        when().
+            post("https://reqres.in/api/users").
+        then().
+            statusCode(HttpStatus.SC_CREATED)
+            .body("name", is ("test"))
+            .body("job", is("QA"))
+            .body("id", is(notNullValue()))
+            .body("createdAt", is(notNullValue()));
+    }
+
+
 
     }
